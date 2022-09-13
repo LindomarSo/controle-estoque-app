7b475dd111ef84cf68a75dd6d18a30abbd3ee6ad
Lindomar <lindomardias21@gmail.com>
2022-09-13 07:14:48 -0300

    feat: first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/user/login/login.component';
+import { UserComponent } from './components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'user/login', pathMatch: 'full' },
+  { path:'user/login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { UserComponent } from './components/user/user.component';
+
+import { NgxSpinnerModule, NgxSpinnerService } from "ngx-spinner";
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { AccountService } from './services/account.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule,
+    // ToastrModule.forRoot({
+    //   progressBar: true,
+    //   timeOut: 3000,
+    //   positionClass: 'toast-bottom-right',
+    //   preventDuplicates: true,
+    // }),
+    NgxSpinnerModule,
   ],
-  providers: [],
+  providers: [
+   // ToastrService,
+    NgxSpinnerService,
+    AccountService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
