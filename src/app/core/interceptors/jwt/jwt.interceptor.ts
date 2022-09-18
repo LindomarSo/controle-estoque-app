6c7c1790ff7760e9ea121b1d1007423bd4b5489b
Gabriel Sousa <gabrielsousa@wizsolucoes.com.br>
2022-09-18 13:23:34 -0300

    feat: criação da autenticação

---
+++ src/app/core/interceptors/jwt/jwt.interceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { AccountService } from '../../services/account/account.service';
+import { User } from 'src/app/shared/models/user.model';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(private accountService: AccountService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    let currentUser: User;
+    this.accountService.currentUser$.pipe(take(1)).subscribe((user: User) => {
+      currentUser = user;
+
+      if (currentUser) {
+        request = request.clone({
+          setHeaders: {
+            Authorization: `Bearer ${currentUser.token}`,
+          },
+        });
+      }
+    });
+    return next.handle(request);
+  }
+}
