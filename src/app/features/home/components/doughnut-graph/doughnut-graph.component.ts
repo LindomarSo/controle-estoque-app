32d9af32171e4f6ace7e3dde0bc6ab9bab6ffbac
Lindomar de Souza Dias <lindomardias21@gmail.com>
2023-02-07 11:44:51 -0300

    chore: implementar gráfico

--- src/app/features/home/components/doughnut-graph/doughnut-graph.component.ts
+++ src/app/features/home/components/doughnut-graph/doughnut-graph.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { DonationService } from 'src/app/core/services/donation/donation.service';
+import { BarChartData } from 'src/app/shared/models/voluntary/barChartData';
 
 @Component({
   selector: 'doughnut-graph',
@@ -6,33 +9,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doughnut-graph.component.scss'],
 })
 export class DoughnutGraphComponent implements OnInit {
-  
+
+  isLoading = false;
+  barChartList: BarChartData[] = [];
+  selectedYear: number = 0;
+  yearsList:number[] = [];
+
   barChartOptions = {
     scaleShowVerticalLines: false,
-    responsive: true
+    responsive: true,
+    options: {
+      scales: {
+        xAxes: [{
+          barPercentage: 0.05
+        }]
+      }
+    }
   };
 
-  barChartLabels = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho'];
+  barChartLabels: string[] = [];
   barChartType = 'bar';
   barChartLegend = true;
   barChartData = [
     {
-      data: [65, 59, 80, 81, 56, 55, 40], label: 'Doações',
+      data: [0], label: 'Doações',
       backgroundColor: '#009AA9'
     },
-    {
-      data: [65, 59, 80, 81, 56, 55, 40], label: 'Voluntários',
-      backgroundColor: '#f44336'
-    }
+    // {
+    //   data: [65, 59, 80, 81, 56, 55, 40], label: 'Voluntários',
+    //   backgroundColor: '#f44336'
+    // }
   ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.initChart();
+  constructor(private donationService: DonationService,
+    private toastr: ToastrService
+  ) {
   }
 
-  initChart() {
+  ngOnInit(): void {
+    this.getLastTenYears();
+    this.getBarChartData(this.selectedYear.toString());
+  }
 
+  getLastTenYears(): void {
+    const currentDate = new Date();
+  
+    for (let i = 0; i < 10; i++) {
+      this.yearsList.push(currentDate.getFullYear() - (10 - i));
+    }
+
+    this.selectedYear = this.yearsList[9];
+  }
+
+  getBarChartData(year: string): void {
+    this.donationService.getBarChartData(`${year}-01-01`).subscribe({
+      next: (response: BarChartData[]) => {
+        this.barChartList = response;
+        this.barChartData[0].data = [];
+        this.barChartLabels = [];
+
+        response.forEach(x => {
+          this.barChartLabels.push(x.mes);
+          this.barChartData[0].data.push(x.value);
+        });
+        
+        this.barChartLabels = this.barChartLabels?.map(str => str.charAt(0).toUpperCase() + str.slice(1, str.length));
+
+        this.isLoading = true;
+      },
+      error: () => {
+        this.toastr.error('Erro ao carregar painel');
+      },
+    });
+  }
+
+  onChange(event: any){
+    this.getBarChartData(event);
   }
 }
