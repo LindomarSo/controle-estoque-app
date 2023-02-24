08898fc99dc6d813b7705c2f7c6ae625c135a833
Gabriel Sousa <gabrielsousa@wizsolucoes.com.br>
2023-02-24 02:52:25 -0300

    feat: página de detalhamento da doação

--- src/app/features/doacoes/donation-updates/donation-updates.component.ts
+++ src/app/features/doacoes/donation-updates/donation-updates.component.ts
@@ -16,6 +16,7 @@ import {
 })
 export class DonationUpdatesComponent {
   public donationForm: FormGroup = new FormGroup({});
+  public donationLoaded: any;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -26,20 +27,50 @@ export class DonationUpdatesComponent {
   ) {}
 
   ngOnInit(): void {
+    this.donationLoaded = this.data.donate;
+
     this.donationForm = new FormGroup({
       id: new FormControl(0),
       tipo: new FormControl('', Validators.required),
       data: new FormControl(new Date(), Validators.required),
-      retiradoPor: new FormControl(new Date(), Validators.required),
+      retiradoPor: new FormControl('', Validators.required),
+      quantidade: new FormControl(0, Validators.pattern('^[0-9]*$')),
     });
   }
 
   sendDonationUpdate() {
-    this.dialogRef.close();
+    this.spinner.show();
 
-    this.toastr.warning(
-      'Falta implementar as atualizações',
-      'Implementar'
-    );
+    let formValue = this.donationForm.getRawValue();
+
+    let updateDonation = {
+      doacaoId: this.donationLoaded.id,
+      quantidade: formValue['quantidade'],
+      tipo: formValue['tipo'].toLowerCase(),
+      retiradoPor: formValue['retiradoPor'],
+      dataRetirada: formValue['data'],
+    };
+
+    console.log('Donation Log: ', updateDonation);
+
+    this.donationService
+      .createUpdateDonation(updateDonation)
+      .subscribe({
+        next: () => {
+          this.toastr.success('Doação atualizada com sucesso!');
+        },
+        error: () => {
+          this.toastr.error('Erro ao atualizar doação.', 'Erro');
+        },
+        complete: () => {
+          this.dialogRef.close();
+          window.location.reload();
+        },
+      })
+      .add(() => {
+        this.spinner.hide();
+      });
+
+    // this.toastr.warning('Falta implementar as atualizações', 'Implementar');
   }
 }
